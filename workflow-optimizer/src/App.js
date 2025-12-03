7c40c2e1efd8fe7dda1fa4bc20996bb1ce147879
NiallSom <niallsomers205@gmail.com>
2025-12-03 22:16:25 +0000

    web-app

---
+++ workflow-optimizer/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import WorkflowOptimizer from './components/WorkflowOptimizer';
+
+function App() {
+  return (
+    <div className="App">
+      <WorkflowOptimizer />
+    </div>
+  );
+}
+
+export default App;
